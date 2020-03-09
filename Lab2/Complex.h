fd86f672e6adfcfdb0521abaf4c7adb8eef2f4d1
Fane276 <stefan.sc.sc@gmail.com>
2020-03-09 19:26:47 +0200

    Added functions name in Complex.h

--- Lab2/Complex.h
+++ Lab2/Complex.h
@@ -1,12 +1,44 @@
 #pragma once
 using namespace std;
 
+
+
 class Complex {
 private:
-	//aici punem fariabile
+	double parte_reala, parte_fractionara;
 public:
 	//constructor implicit
 	Complex();
+
+	//constructor de copiere
+	Complex(const Complex&);
+
+	//functia de copiere
+	Complex operator= (Complex c);
+
+	//returneaza ceva
+	double show_comp();
+
+	//returneaza ceva
+	double show_exp();
+
+	//Face adunarea
+	Complex operator+ (Complex c);
+
+	//Face inmultire
+	Complex operator* (Complex c);
+	
+	//Face inpartire
+	Complex operator/ (Complex c);
+
+	//ABS
+	double abs();
+
+	//De adaugat polar form
+
+
+	//destructor
+	~Complex();
 
 	//aici punem metodele
 
